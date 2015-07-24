bd2339018f0c33a8527c295bf25f012a8afd18ae
Oleksiy_Prosyanko <oleksiyprosyanko@gmail.com>
2015-07-24 16:29:22 +0300

    Refactoring: ServerSuite duplication removed.

--- src/test/scala/op/trial/akka/ServerSuite.scala
+++ src/test/scala/op/trial/akka/ServerSuite.scala
@@ -4,6 +4,7 @@ import java.net.{InetSocketAddress, Socket}
 import akka.actor.{Props, ActorSystem}
 import akka.routing.{Listen, Listeners}
 import akka.testkit.TestKit
+import com.ning.http.client.Response
 import org.scalatest.{BeforeAndAfterEach, FunSuiteLike, BeforeAndAfterAll}
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -37,44 +38,59 @@ class ServerSuite extends TestKit(ActorSystem("ServerSuite"))
     }
   }
 
+  test("receive 'Service' messages when new HTTP requests received") {
+    val server = testServer(
+      mappings = Map("/foo" -> Props(WorkerMock)), name = "test-actor-messages-server")
+
+    Http(url(s"http://localhost:$port/$app/foo"))
+    val messages = receiveWhile() {
+      case srv@Service(wp, ex) => srv
+    }
+    assert(messages.size == 1)
+    system stop server
+  }
+
   test("server should receive HTTP requests") {
-    val server = system.actorOf(Props(
-      new ServerActor(app, port,
-        mappings = Map(
-          "/foo" -> Props(WorkerMock),
-          "/error" -> Props(ErrorWorker)
-        )
-      ) with Listeners {
-        override val receive: Receive = listenerManagement orElse {
-          case msg => super.receive(msg); gossip(msg)
-        }
-      }
-    ), "server-receive-http")
-    server ! Listen(testActor)
+    val server = testServer(
+      mappings = Map(
+      "/foo" -> Props(WorkerMock),
+      "/error" -> Props(ErrorWorker)
+    ),"server-receive-http")
 
     withClue("success") {
-      val req = url(s"http://localhost:$port/$app/foo")
-      val resp = Http(req)
-      assert(Await.result(resp, 1 second).getStatusCode == 200)
-      assert(Await.result(resp, 0 second).getResponseBody == "well done")
+      val resp = Http(url(s"http://localhost:$port/$app/foo"))
+      val result = Await.result(resp, 1 second)
+      assert(result.getStatusCode == 200)
+      assert(result.getResponseBody == "well done")
       assert (expectMsgPF() {
         case Service(wp ,ex) => true
         case _ => false
       })
     }
     withClue("error response status in case of internal error") {
-      val req = url(s"http://localhost:$port/$app/error")
-      val resp = Http(req)
-      assert(Await.result(resp, 1 second).getStatusCode == 500)
-      assert(Await.result(resp, 0 second).getResponseBody == "error message")
+      val resp = Http(url(s"http://localhost:$port/$app/error"))
+      val result = Await.result(resp, 1 second)
+      assert(result.getStatusCode == 500)
+      assert(result.getResponseBody == "error message")
     }
     withClue("not found") {
-      val req = url(s"http://localhost:$port/$app/not_found")
-      val resp = Http(req)
+      val resp = Http(url(s"http://localhost:$port/$app/not_found"))
       assert(Await.result(resp, 1 second).getStatusCode == 404)
     }
 
     system stop server
+  }
+
+  private def testServer(mappings: Map[String, Props], name: String) = {
+    val server = system.actorOf(Props(
+      new ServerActor(app, port, mappings) with Listeners {
+        override val receive: Receive = listenerManagement orElse {
+          case msg => super.receive(msg); gossip(msg)
+        }
+      }
+    ), name)
+    server ! Listen(testActor)
+    server
   }
 }
 
