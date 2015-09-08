fb2b15261404a798397e9a4c8fd0fa1918ccd3a6
Oleksiy Prosyanko <oleksiy_prosyanko@epam.com>
2015-09-08 16:45:04 +0300

    Test case implemented: when ServerCluster starts  => start server actor.

--- src/main/scala/op/trial/akka/ServerCluster.scala
+++ src/main/scala/op/trial/akka/ServerCluster.scala
@@ -1,11 +1,12 @@
 package op.trial.akka
 
-import akka.actor.Actor
+import akka.actor.{Props, Actor}
 import akka.cluster.{ClusterEvent, Cluster}
 
-class ServerCluster extends Actor {
+class ServerCluster(serverProps: Props) extends Actor {
   val cluster  = Cluster(context.system)
   cluster.join(cluster.selfAddress)
+  context.actorOf(serverProps)
 
-  def receive: Receive = { case _ => () }
+  def receive: Receive = {case _ => ()}
 }
--- src/test/scala/op/trial/akka/ScalabilitySuite.scala
+++ src/test/scala/op/trial/akka/ScalabilitySuite.scala
@@ -12,38 +12,37 @@ class ScalabilitySuite extends TestKit(ActorSystem("ScalabilitySuite"))
 
   import ScalabilitySuite._
 
-  val serverCluster = system.actorOf(Props(new TestCluster(testActor)), "server-cluster")
+  def serverProps = Props(new FakeServer(testActor))
+  val serverCluster = system.actorOf(Props(new TestCluster(testActor, serverProps)), "server-cluster")
+
   override protected def afterAll() =  {
     system stop serverCluster
-    system.shutdown()
+    shutdown()
   }
 
   "Server cluster" must {
-    "joint self to a single node cluster" in {
+    "joint self to a single node cluster and start server actor" in {
+      expectMsg(ServerActorStarted)
       expectMsgPF() {
         case state: ClusterEvent.CurrentClusterState => assert(state.members.size == 1)
-      }
-      expectMsgPF() {
-        case ClusterEvent.MemberUp(member) => ()
       }
     }
   }
 }
 
 object ScalabilitySuite {
-  class TestCluster(probe: ActorRef) extends ServerCluster {
-    cluster.subscribe(self, classOf[ClusterEvent.MemberUp])
+  case object ServerActorStarted
+
+  class TestCluster(probe: ActorRef, serverProps: Props) extends ServerCluster(serverProps) {
+    cluster.subscribe(self, classOf[Any])
     override def receive: Receive = { case msg => probe ! msg; super.receive(msg) }
   }
-
-  import op.trial.akka.util.FakeLifeCicleAware
   class FakeWorker() extends WorkerActor {
     def work() {}
   }
-  class FakeServer extends ServerActor with FakeLifeCicleAware {
-    def success(res: Any, exchange: Unit) {}
-    def failure(cause: Throwable, exchange: Unit) {}
-    def receive: Actor.Receive = { case _ => () }
+  class FakeServer(probe: ActorRef) extends Actor {
+    probe ! ServerActorStarted
+    val receive: Actor.Receive = { case _ => () }
   }
 }
 
