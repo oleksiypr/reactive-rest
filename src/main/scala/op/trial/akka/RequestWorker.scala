32f21bb0fca5a101a6ed6724fbf1d7c50478b2a4
Oleksiy Prosyanko <oleksiy_prosyanko@epam.com>
2015-09-09 12:51:11 +0300

    Refactoring: changed access modifier of RequestWorker#work to protected.

--- src/main/scala/op/trial/akka/RequestWorker.scala
+++ src/main/scala/op/trial/akka/RequestWorker.scala
@@ -6,7 +6,7 @@ import scala.util.Try
 class RequestWorker[U, T](function: U => T, arg: U) extends Actor {
   work()
 
-  def work() {
+  protected[this] def work() {
     val resultTry = Try(function(arg))
     context.parent ! resultTry
     context stop self
